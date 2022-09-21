66fb3fe8f0a9e18855dada1f5417954dc55f887d
ozendorf <50991080+ozendorf@users.noreply.github.com>
2022-09-21 10:15:46 +0200

    rework for defining a function + starting debuging

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import json
+import sys
 
-with open('./roledefinition', 'r') as f:
-  roledefinition = json.load(f)
-
-# Output: {'name': 'Bob', 'languages': ['English', 'French']}
-print(json.dumps(roledefinition, indent=4))
+def used_actions(user):
+  with open('./activity_log_full', 'r') as f:
+    logs = json.load(f)
+  print(user)
+  n = 0
+  for i in logs:
+    if i['caller'] == user:
+      n = n + 1
+      print('The caller is ', user)
+      print(json.dumps(i['authorization']['action'], indent=4))
+      action = i['authorization']['action']
+      counter = action
+      counter[action] += 1
+      
+counter = {}
+user = sys.argv[1]
+used_actions(user)
+print(counter)
