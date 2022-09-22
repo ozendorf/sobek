1182756687b0da5fffd090ecbaacca157c68e6f9
ozendorf <50991080+ozendorf@users.noreply.github.com>
2022-09-22 08:59:22 +0200

    counting occurence of actions

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 import json
 import sys
+counter = {}
 
 def used_actions(user):
   with open('./activity_log_full', 'r') as f:
     logs = json.load(f)
   print(user)
-  n = 0
+  counter = {}
   for i in logs:
     if i['caller'] == user:
-      n = n + 1
       print('The caller is ', user)
       print(json.dumps(i['authorization']['action'], indent=4))
-      action = i['authorization']['action']
-      counter = action
-      counter[action] += 1
+      action = i['authorization']['action']   
+      counter[action] = counter.get(action, 0) +1
+  print('counter result is:')
+  print(counter)
+  return counter
       
-counter = {}
 user = sys.argv[1]
 used_actions(user)
+print('counter result is:')
 print(counter)
