3273e9b6a2589e64cf9752e266e84a1c9ae99647
ozendorf <50991080+ozendorf@users.noreply.github.com>
2022-09-28 08:39:17 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -9,15 +9,25 @@ def used_actions(user):
   counter = {}
   for i in logs:
     if i['caller'] == user:
-      print('The caller is ', user)
-      print(json.dumps(i['authorization']['action'], indent=4))
+      #print('The caller is ', user)
+      #print(json.dumps(i['authorization']['action'], indent=4))
       action = i['authorization']['action']   
       counter[action] = counter.get(action, 0) +1
-  print('counter result is:')
-  print(counter)
   return counter
-      
+
+def assigned_roles(user):
+  with open('./role_assignments', 'r') as f:
+    roles_file = json.load(f)
+    roles = {}
+  n=0  
+  for i in roles_file:
+    n += 1
+    roles[n] = i['roleDefinitionName']
+  return roles  
+  
 user = sys.argv[1]
-used_actions(user)
-print('counter result is:')
-print(counter)
+counter = used_actions(user)
+#print('counter result is:')
+#print(counter)
+roles = assigned_roles(user)
+print(roles)
